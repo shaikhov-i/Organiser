40e5172c02a39211a4eab787f32b546c731e83d9
shaikhov-i <65728594+shaikhov-i@users.noreply.github.com>
2020-10-06 19:04:06 +0500

    Added functions. Organiser is working

--- main.js
+++ main.js
@@ -4,11 +4,11 @@ let table = document.getElementById('table')
 init(table)
 
 function init(table) {
+    let activities = []
     let days = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
     let now = new Date()
-    let date = new Date(now.getFullYear(), now.getMonth() + 1, 0)
-    
-    
+    let date = new Date(now.getFullYear(), now.getMonth() + 3, 0)
+    let parentField = document.querySelector('.parent')
     fillTable(table, days, date)
 
 
@@ -19,22 +19,102 @@ function init(table) {
         let row = createRows(table, 5)
         let col = createCols(row, days.length)
         fillDaysRow(daysRow[0].querySelectorAll('td'), days)
-        fillDateRow(col, dayInMonth, days, date)
+        fillDateRow(col, dayInMonth, days, date, now)
     }
-    function fillDateRow(col, day, days, date) {
-        for (let i = 0; i < col.length; i++) {
-            
+
+    function fillDateRow(col, day, days, date, now) {
+        let activities = {}
+        let count = 1
+
+        let workingCols = sortByDay(col, count, date)
+
+
+        for (let i = 0; i < workingCols.length; i++) {
+            workingCols[i].classList.add('active')
+            workingCols[i].addEventListener('click', function () {
+                let data
+                if (!activities[workingCols[i].innerHTML]) {
+                    data = activities[workingCols[i].innerHTML] = []
+                } else {
+                    data = activities[workingCols[i].innerHTML]
+                }
+
+                parentField.innerHTML = `
+                <div id="field"></div>
+                <div id="create-list">
+                    <input type="text" id="input">
+                    <button id="btn">Добавить</button>
+                    <button id="close-btn">Закрыть</button>
+                </div>
+                `
+                console.log(data)
+
+                let field = parentField.querySelector('#field')
+                if (activities[workingCols[i].innerHTML].length != 0) {
+                    for (let i = 0; i < data.length; i++) {
+                        createElement(field, data[i], i)
+                    }
+                }
+
+                console.log(activities)
+                let input = parentField.querySelector('#input')
+                let btn = parentField.querySelector('#btn')
+                let closeBtn = parentField.querySelector('#close-btn')
+
+                function createElement(field, data, i) {
+                    let p = document.createElement('p')
+                    p.innerHTML = i + 1 + '. ' + data
+                    field.appendChild(p)
+                }
+                btn.addEventListener('click', function () {
+                    let p = document.createElement('p')
+                    p.innerHTML = data.length + 1 + '. ' + input.value
+                    data.push(input.value)
+                    input.value = ''
+                    field.appendChild(p)
+                    console.log(activities[workingCols[i].innerHTML])
+                })
+                closeBtn.addEventListener('click', function () {
+                    parentField.innerHTML = ''
+                })
+            })
         }
     }
+
+
+
+    function sortByDay(col, count, date) {
+        let cols = []
+        let firstDay = new Date(date.getFullYear(), date.getMonth(), 1)
+        let d = firstDay.getDay()
+        if (d == 0) {
+            d = 7
+        }
+        for (let i = 0; i < col.length; i++) {
+            if (i < d - 1) {
+                col[i].innerHTML = ''
+            } else {
+                if (count > date.getDate()) {} else {
+                    col[i].innerHTML = count
+                    cols.push(col[i])
+                    count++
+                }
+            }
+        }
+        return cols
+    }
+
     function fillDaysRow(rowArr, days) {
         for (let i = 0; i < rowArr.length; i++) {
             rowArr[i].innerHTML = days[i]
         }
     }
+
     function createDaysRow(table) {
         let tr = createRows(table, 1)
         return tr
     }
+
     function createRows(table, count) {
         let rows = []
         for (let i = 0; i < count; i++) {
@@ -44,6 +124,7 @@ function init(table) {
         }
         return rows
     }
+
     function createCols(rowArr, count) {
         let cols = []
         for (let i = 0; i < rowArr.length; i++) {
